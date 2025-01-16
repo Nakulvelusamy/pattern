8b1ac20cd62164c71df3194af2ff537f16a5e729
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-16 16:10:05 +0530

    Create Rectangular Star Pattern.java

---
+++ Rectangular Star Pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      patern(6);
+    }
+      static void patern(int n){
+        for (int row=0;row<n;row++){
+          for (int col=0;col<n;col++){
+              System.out.print("* ");
+          }
+          System.out.println();
+        }
+      }
+  }
